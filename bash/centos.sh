8665b5c82b36fe4d127fe4690d333a6ec6f4952e
Lyndon <snakeliwei@gmail.com>
2020-08-24 15:52:36 +0800

    Create centos.sh

---
+++ bash/centos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# install elrepo
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
+
+# install kernel-ml
+yum update -y
+yum --enablerepo=elrepo-kernel install kernel-ml -y
+
+# add BBR config
+bash -c 'echo "# Enable BBR" >> /usr/lib/sysctl.d/50-default.conf'
+bash -c 'echo "net.core.default_qdisc = fq" >> /usr/lib/sysctl.d/50-default.conf'
+bash -c 'echo "net.ipv4.tcp_congestion_control=bbr" >> /usr/lib/sysctl.d/50-default.conf'
+
+# modify grub menu
+grub2-set-default 0
+grub2-mkconfig -o /boot/grub2/grub.cfg
